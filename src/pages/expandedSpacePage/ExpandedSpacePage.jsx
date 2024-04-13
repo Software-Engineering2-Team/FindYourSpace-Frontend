28baf872c75db47a7628caf3ff54f01a64f905ec
AkhilAjith2 <akhilajith2003@gmail.com>
2024-04-13 13:30:17 +0200

    all customer pages

--- src/pages/expandedSpacePage/ExpandedSpacePage.jsx
+++ src/pages/expandedSpacePage/ExpandedSpacePage.jsx
@@ -15,7 +15,6 @@
   import AddIcon from '@mui/icons-material/Add';
   import RemoveIcon from '@mui/icons-material/Remove';
   import axios from 'axios';
-  import { Link } from 'react-router-dom';
   const defaultTheme = createTheme({
     palette: {
       primary: {
