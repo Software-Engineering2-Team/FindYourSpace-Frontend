ed4f3560a2bdf14c0f22635219a885371b6e2d0e
David Abraham <davidabraham384@gmail.com>
2024-06-04 16:32:03 +0200

    add review functionality

--- src/__tests__/AddReview.test.jsx
+++ src/__tests__/AddReview.test.jsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import AddReviewForm from '../pages/reviews/AddReview';
 
-test('renders SearchMySpaces component', () => {
+test('renders AddReviewForm component', () => {
     render(
       <MemoryRouter>
         <AddReviewForm/>
