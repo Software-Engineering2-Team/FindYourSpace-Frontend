e5b64a80bdf2d3dd9351a4f13182830720370597
AkhilAjith2 <akhilajith2003@gmail.com>
2025-01-12 19:43:43 +0100

    deleted test

--- src/__tests__/List.test.jsx
+++ src/__tests__/List.test.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import List from '../pages/list/List';
+import React from "react";
+import { render, screen, fireEvent, act } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import List from "../pages/list/List";
 
-describe('List Component', () => {
-  test('renders list without crashing', async () => {
+describe("List Component", () => {
+  test("renders list without crashing", async () => {
     await act(async () => {
       render(
         <MemoryRouter>
@@ -12,7 +12,7 @@ describe('List Component', () => {
         </MemoryRouter>
       );
     });
-    const listElement = screen.getByTestId('list-1');
+    const listElement = screen.getByTestId("list-1");
     expect(listElement).toBeInTheDocument();
   });
 });
